0aa6d7b39f5779e917cc91ab6f77afbb52dbd0a8
mahendrabhikyagolu <mahendra81@gmail.com>
2019-03-02 15:38:11 +0530

    client react added

---
+++ src/main/resources/static/precache-manifest.350c1c40c68dadda9a178a0801f43482.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "c285e78fa64424782454",
+    "url": "/static/js/main.c285e78f.chunk.js"
+  },
+  {
+    "revision": "4bb59e04a4f2f313b2bf",
+    "url": "/static/js/2.4bb59e04.chunk.js"
+  },
+  {
+    "revision": "c285e78fa64424782454",
+    "url": "/static/css/main.c898b1f1.chunk.css"
+  },
+  {
+    "revision": "4bb59e04a4f2f313b2bf",
+    "url": "/static/css/2.e5ea9e58.chunk.css"
+  },
+  {
+    "revision": "e458ba6c8a1fc77321bba0b980d0d45f",
+    "url": "/index.html"
+  }
+];
